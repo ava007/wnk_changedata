9a275791a8338d47611b7ec41ee7d841b1e2ebd3
ava007 <avonarx@gmx.net>
2019-03-19 19:22:12 +0100

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,6 +4,6 @@ use App\Controller\AppController as BaseController;
 class AppController extends BaseController {
     public function initialize() {
         parent::initialize();
-        $this->viewBuilder()->layout('WnkTranslation.default');
+     //   $this->viewBuilder()->layout('WnkTranslation.default');
     }
 }
